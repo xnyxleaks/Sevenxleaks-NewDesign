a1b66011972ba71f3350d354a4a4a4026b4c78e3
ruandevz <Ruanbatista1509@outlook.com>
2025-08-15 00:00:21 -0300

    fix: authorize host

--- BACKEND/Middleware/CheckapiKey.js
+++ BACKEND/Middleware/CheckapiKey.js
@@ -7,6 +7,12 @@ module.exports = function checkApiKey(req, res, next) {
   const allowedApiKey = process.env.VITE_FRONTEND_API_KEY;
   const allowedAdminKey = process.env.ADMIN_API_KEY;
 
+  const allowedOrigins = [
+    'https://sevenxleaks.com',
+    'http://localhost:5173',
+    'https://newdesign-livid.vercel.app'
+  ];
+
   const isAllowedOrigin =
     allowedOrigins.includes(origin) ||
     allowedOrigins.some(o => referer?.startsWith(o));
