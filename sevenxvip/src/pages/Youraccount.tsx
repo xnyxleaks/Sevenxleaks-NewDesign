32bc6a1eefeba3289a90df9458355d4bdf49b1f0
ruandevz <Ruanbatista1509@outlook.com>
2025-09-03 23:12:02 -0300

    feat: remove favorite content

--- sevenxvip/src/pages/Youraccount.tsx
+++ sevenxvip/src/pages/Youraccount.tsx
@@ -179,13 +179,13 @@ const YourAccount: React.FC = () => {
               </motion.div>
             )}
             
-            <motion.div
+            {/* <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.5 }}
             >
               <FavoritesSection favorites={userData.favorites} />
-            </motion.div>
+            </motion.div> */}
           </div>
           
           {/* Right Column - Sidebar */}
